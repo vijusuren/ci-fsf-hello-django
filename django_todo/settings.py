9f3d840ca85b32e18fc2e470176c16e690f8cf09
vijusuren <vijayalakshmioviya@gmail.com>
2021-07-04 09:23:52 +0000

    add development in setting.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 import dj_database_url
 
-development = os.environ.get('DEVELEOPMENT', False)
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
